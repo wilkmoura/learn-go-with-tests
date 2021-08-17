292ac0fa62c39e3f201bce35a192faaf89ebfe0b
Wilkinson Tavares <wilkmoura@gmail.com>
2021-08-17 13:09:57 -0300

    feat: specify how many times the char is repeated

--- iteration/iteration.go
+++ iteration/iteration.go
@@ -1,8 +1,8 @@
 package iteration
 
-func Repeat(character string) string {
+func Repeat(character string, numRepeatition int) string {
 	var repeated string
-	for i := 0; i < 5; i++ {
+	for i := 0; i < numRepeatition; i++ {
 		repeated = repeated + character
 	}
 	return repeated
--- iteration/iteration_test.go
+++ iteration/iteration_test.go
@@ -1,10 +1,13 @@
 package iteration
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+)
 
 func TestRepeat(t *testing.T) {
-	repeated := Repeat("a")
-	expected := "aaaaa"
+	repeated := Repeat("a", 10)
+	expected := "aaaaaaaaaa"
 
 	if repeated != expected {
 		t.Errorf("expected %q but got %q", expected, repeated)
@@ -13,6 +16,6 @@ func TestRepeat(t *testing.T) {
 
 func BenchmarkRepeat(b *testing.B) {
 	for i := 0; i < b.N; i++ {
-		Repeat("a")
+		Repeat("a", 3)
 	}
 }
