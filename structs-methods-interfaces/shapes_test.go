7755787b4cf940fff248a4376d15a440640b5cac
Wilkinson Tavares <wilkmoura@gmail.com>
2021-08-20 11:51:46 -0300

    refactor: table driven tests for better readability

--- structs-methods-interfaces/shapes_test.go
+++ structs-methods-interfaces/shapes_test.go
@@ -13,25 +13,20 @@ func TestPerimeter(t *testing.T) {
 }
 
 func TestArea(t *testing.T) {
-	checkArea := func(t testing.TB, shape Shape, want float64) {
-		t.Helper()
-		got := shape.Area()
-		if got != want {
-			t.Errorf("got %.2g want %.2g", got, want)
-		}
+	areaTests := []struct {
+		shape Shape
+		want  float64
+	}{
+		{Rectangle{10.0, 8.0}, 80.0},
+		{Circle{10.0}, 314.1592653589793},
 	}
 
-	t.Run("rectangle area", func(t *testing.T) {
-		rectangle := Rectangle{10.0, 8.0}
-		want := 80.0
-		checkArea(t, rectangle, want)
-	})
-
-	t.Run("circle area", func(t *testing.T) {
-		circle := Circle{10}
-		want := 314.1592653589793
-		checkArea(t, circle, want)
-	})
+	for _, tt := range areaTests {
+		got := tt.shape.Area()
+		if got != tt.want {
+			t.Errorf("got %.2g want %.2g", got, tt.want)
+		}
+	}
 }
 
 /*
