7ae872d3996f669b84d80808d54ce826c96028f6
Wilkinson Tavares <wilkinson@proabakus.com.br>
2021-08-20 00:04:35 -0300

    feat: calculate the total of tails of each slice

--- arrays-and-slices/sum_test.go
+++ arrays-and-slices/sum_test.go
@@ -34,3 +34,24 @@ but rather to have as much confidence as possible in your code base.
 Having too many tests can turn in to a real problem and it just adds more overhead in maintenance.
 EVERY TEST HAS A COST.
 */
+
+func TestSumAllTails(t *testing.T) {
+	checkSums := func(t testing.TB, got, want []int) {
+		t.Helper()
+		if !reflect.DeepEqual(got, want) {
+			t.Errorf("got %v want %v", got, want)
+		}
+	}
+
+	t.Run("make the sums of some slices", func(t *testing.T) {
+		got := SumAllTails([]int{1, 2}, []int{0, 9})
+		want := []int{2, 9}
+		checkSums(t, got, want)
+	})
+
+	t.Run("safely sum empty slices", func(t *testing.T) {
+		got := SumAllTails([]int{}, []int{3, 4, 5})
+		want := []int{0, 9}
+		checkSums(t, got, want)
+	})
+}
