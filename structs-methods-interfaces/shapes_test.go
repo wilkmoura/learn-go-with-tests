e747c28837a0a2dc662d623cb5324000f34fdc22
Wilkinson Tavares <wilkmoura@gmail.com>
2021-08-20 11:29:32 -0300

    feat: calculate area of a circle

--- structs-methods-interfaces/shapes_test.go
+++ structs-methods-interfaces/shapes_test.go
@@ -13,11 +13,23 @@ func TestPerimeter(t *testing.T) {
 }
 
 func TestArea(t *testing.T) {
-	rectangle := Rectangle{10.0, 8.0}
-	got := Area(rectangle)
-	want := 80.0
+	t.Run("rectangle area", func(t *testing.T) {
+		rectangle := Rectangle{10.0, 8.0}
+		got := rectangle.Area()
+		want := 80.0
 
-	if got != want {
-		t.Errorf("got %.2f want %.2f", got, want)
-	}
+		if got != want {
+			t.Errorf("got %.2g want %.2g", got, want)
+		}
+	})
+
+	t.Run("circle area", func(t *testing.T) {
+		circle := Circle{10}
+		got := circle.Area()
+		want := 314.1592653589793
+
+		if got != want {
+			t.Errorf("got %.2g want %.2g", got, want)
+		}
+	})
 }
