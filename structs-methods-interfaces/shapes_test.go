d8f11e551234610681c78fd75679cf114f31cf2e
Wilkinson Tavares <wilkmoura@gmail.com>
2021-08-20 11:10:57 -0300

    feat: calculate area of a rectangle

--- structs-methods-interfaces/shapes_test.go
+++ structs-methods-interfaces/shapes_test.go
@@ -10,3 +10,12 @@ func TestPerimeter(t *testing.T) {
 		t.Errorf("got %.2f want %.2f", got, want)
 	}
 }
+
+func TestArea(t *testing.T) {
+	got := Area(10.0, 8.0)
+	want := 80.0
+
+	if got != want {
+		t.Errorf("got %.2f want %.2f", got, want)
+	}
+}
