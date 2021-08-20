8cfb7501a5d6bb949adf5ce694c8071d37b9e493
Wilkinson Tavares <wilkmoura@gmail.com>
2021-08-20 11:35:32 -0300

    refactor: shape interface

--- structs-methods-interfaces/shapes_test.go
+++ structs-methods-interfaces/shapes_test.go
@@ -13,23 +13,29 @@ func TestPerimeter(t *testing.T) {
 }
 
 func TestArea(t *testing.T) {
-	t.Run("rectangle area", func(t *testing.T) {
-		rectangle := Rectangle{10.0, 8.0}
-		got := rectangle.Area()
-		want := 80.0
-
+	checkArea := func(t testing.TB, shape Shape, want float64) {
+		t.Helper()
+		got := shape.Area()
 		if got != want {
 			t.Errorf("got %.2g want %.2g", got, want)
 		}
+	}
+
+	t.Run("rectangle area", func(t *testing.T) {
+		rectangle := Rectangle{10.0, 8.0}
+		want := 80.0
+		checkArea(t, rectangle, want)
 	})
 
 	t.Run("circle area", func(t *testing.T) {
 		circle := Circle{10}
-		got := circle.Area()
 		want := 314.1592653589793
-
-		if got != want {
-			t.Errorf("got %.2g want %.2g", got, want)
-		}
+		checkArea(t, circle, want)
 	})
 }
+
+/*
+In Go interface resolution is implicit.
+If the type you pass in matches what the interface is asking for,
+it will compile
+*/
