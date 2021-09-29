924abd3d068541d16d2aa50a0a2d7d517caed8f2
Wilkinson Tavares <wilkmoura@gmail.com>
2021-09-28 22:17:56 -0300

    feat: improving test and decoupling with Dependency Injection and Mock

--- mocking/countdown.go
+++ mocking/countdown.go
@@ -10,17 +10,26 @@ import (
 const finalWord = "Go!"
 const countdownStart = 3
 
-func Countdown(out io.Writer) {
+type Sleeper interface {
+	Sleep()
+}
+
+type DefaultSleeper struct{}
+
+func (d *DefaultSleeper) Sleep() {
+	time.Sleep(1 * time.Second)
+}
+
+func Countdown(out io.Writer, sleeper Sleeper) {
 	for i := countdownStart; i > 0; i-- {
-		time.Sleep(1 * time.Second)
+		sleeper.Sleep()
 		fmt.Fprintln(out, i)
 	}
-	time.Sleep(1 * time.Second)
+	sleeper.Sleep()
 	fmt.Fprintf(out, finalWord)
 }
 
 func main() {
-	Countdown(os.Stdout)
+	sleeper := &DefaultSleeper{}
+	Countdown(os.Stdout, sleeper)
 }
-
-// stopped at https://quii.gitbook.io/learn-go-with-tests/go-fundamentals/mocking#write-the-test-first-2
