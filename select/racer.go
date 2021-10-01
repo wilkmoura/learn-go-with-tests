43d8184ca3667de0d0eedd02eb78b43ad0d51d98
Wilkinson Tavares <wilkmoura@gmail.com>
2021-09-30 22:02:21 -0300

    refactor: use select to get the fastest server and goroutine to async request

--- select/racer.go
+++ select/racer.go
@@ -1,23 +1,28 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
 	"time"
 )
 
-func measureResponseTime(url string) time.Duration {
-	start := time.Now()
-	http.Get(url)
-	return time.Since(start)
+func Racer(a, b string, timeout time.Duration) (winner string, err error) {
+	select {
+	case <-ping(a):
+		return a, nil
+	case <-ping(b):
+		return b, nil
+	case <-time.After(timeout):
+		return "", fmt.Errorf("timed out waiting for %s and %s", a, b)
+	}
 }
 
-func Racer(a, b string) (winner string) {
-	aDuration := measureResponseTime(a)
-	bDuration := measureResponseTime(b)
+func ping(url string) chan struct{} {
+	ch := make(chan struct{})
+	go func() {
+		http.Get(url)
+		close(ch)
+	}()
 
-	if aDuration < bDuration {
-		return a
-	}
-
-	return b
+	return ch
 }
--- select/racer_test.go
+++ select/racer_test.go
@@ -8,21 +8,41 @@ import (
 )
 
 func TestRacer(t *testing.T) {
-	slowServer := makeDelayedServer(20 * time.Millisecond)
-	fastServer := makeDelayedServer(0 * time.Millisecond)
+	t.Run("races two servers", func(t *testing.T) {
+		slowServer := makeDelayedServer(20 * time.Millisecond)
+		fastServer := makeDelayedServer(0 * time.Millisecond)
 
-	defer slowServer.Close()
-	defer fastServer.Close()
+		defer slowServer.Close()
+		defer fastServer.Close()
 
-	slowURL := slowServer.URL
-	fastURL := fastServer.URL
+		slowURL := slowServer.URL
+		fastURL := fastServer.URL
 
-	want := fastURL
-	got := Racer(slowURL, fastURL)
+		want := fastURL
+		got, err := Racer(slowURL, fastURL, 1*time.Second)
 
-	if got != want {
-		t.Errorf("got %q, want %q", got, want)
-	}
+		if got != want {
+			t.Errorf("got %q, want %q", got, want)
+		}
+
+		if err != nil {
+			t.Fatalf("it shouldn't have an error %v", err)
+		}
+	})
+
+	t.Run("return an error if a server doesn't response within 10s", func(t *testing.T) {
+		serverA := makeDelayedServer(11 * time.Millisecond)
+		serverB := makeDelayedServer(12 * time.Millisecond)
+
+		defer serverA.Close()
+		defer serverB.Close()
+
+		_, err := Racer(serverA.URL, serverB.URL, 5*time.Millisecond)
+
+		if err == nil {
+			t.Errorf("expect an error but didn't get one")
+		}
+	})
 }
 
 func makeDelayedServer(delay time.Duration) *httptest.Server {
